c4160d5c4aaf6cf384475f4d7e26953e76f15be9
Duco Lam <d.f.lam@student.tudelft.nl>
2024-02-22 14:51:28 +0100

    Finished the Date and its Test class!

--- commons/src/test/java/commons/DateTest.java
+++ commons/src/test/java/commons/DateTest.java
@@ -27,24 +27,17 @@ class DateTest {
         assertNotEquals(d1, d2);
     }
 
-//    @Test
-//    void testHashCode() {
-//    }
+    @Test
+    void testHashCode1() {
+        Date d1 = new Date(123,1,1,2024,24,24);
+        Date d2 = new Date(123,1,1,2024,24,24);
+        assertTrue(d1.hashCode() == d2.hashCode());
+    }
 
     @Test
-    void testToString() {
+    void testHashCode2() {
         Date d1 = new Date(123,1,1,2024,24,24);
-        String testResult = d1.toString();
-        String result = "commons.Date@7c2a69b4[\n" +
-                "  day=1\n" +
-                "  hour=24\n" +
-                "  id=123\n" +
-                "  minute=24\n" +
-                "  month=1\n" +
-                "  year=2024\n" +
-                "]";
-        System.out.println(testResult.substring(21));
-        System.out.println(result.substring(21));
-        assertTrue(testResult.substring(21).equals(result.substring(21)));
+        Date d2 = new Date(124,1,1,2024,24,24);
+        assertFalse(d1.hashCode() == d2.hashCode());
     }
 }
