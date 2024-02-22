58e8740b0702d6d34b2a196cae337b23f47358df
Frank Verkoren <f.h.w.verkoren@student.tudelft.nl>
2024-02-22 22:29:39 +0100

    Fixed checkstyle warning unused imports

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -1,13 +1,8 @@
 package commons;
 
-import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
-
 import jakarta.persistence.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-
-import java.util.*;
 
 @Entity
 public class User {
