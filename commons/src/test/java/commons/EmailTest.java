dea21748e538c56dc5dc057a6bdd723c2267a701
Vlad Anica <V.I.Anica-Popa@student.tudelft.nl>
2024-02-24 01:12:59 +0100

    Added tests for the Email class

--- commons/src/test/java/commons/EmailTest.java
+++ commons/src/test/java/commons/EmailTest.java
@@ -1,4 +1,48 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmailTest {
-  
+    private static Email email1 = new Email("1");
+    private static Email email2 = new Email("2");
+    private static Email email3 = new Email("3");
+    private static String beginEmail1 = "Email: 1; ";
+
+    @Test
+    void getAddress() {
+        assertTrue(email1.getAddress().equals("1"));
+    }
+
+    @Test
+    void setAddress() {
+        email1.setAddress("4");
+        assertTrue(email1.getAddress().equals("4"));
+    }
+
+    @Test
+    void testEquals() {
+        assertFalse(email1.equals(email2));
+    }
+
+    @Test
+    void testHashCode() {
+        assertTrue(email1.hashCode() == email3.hashCode());
+    }
+
+    @Test
+    void equalsWithoutIdFalse() {
+        assertFalse(email1.equalsWithoutId(email2));
+    }
+
+    @Test
+    void equalsWithoutIdTrue() {
+        assertTrue(email1.equalsWithoutId(email3));
+    }
+
+    @Test
+    void testToString() {
+        assertTrue(email1.toString().startsWith(beginEmail1));
+    }
 }
