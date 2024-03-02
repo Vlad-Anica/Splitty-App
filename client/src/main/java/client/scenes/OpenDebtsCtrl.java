1fb9347c3afc8f314bcc97229e74c4113ab5fea1
Duco Lam <d.f.lam@student.tudelft.nl>
2024-03-02 15:38:52 +0100

    Added Home button to the open depths scene!

--- client/src/main/java/client/scenes/OpenDebtsCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsCtrl.java
@@ -3,7 +3,15 @@ package client.scenes;
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /***
  * Unused imports:
  * import javafx.scene.control.Label;
@@ -12,15 +20,40 @@ import javafx.scene.control.Button;
  */
 
 
+
+
 public class OpenDebtsCtrl {
+    @FXML
+    private Button goHomeButton;
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
     @FXML
     private Button backButton;
 
     //need a way to show open debts from the database
 
-    @FXML
-    public void goBack(ActionEvent event) {
 
+    //For Now, the back button is the same as the home button, this needs changing in the future!
+    @FXML
+    public void goBack(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
         System.out.println("go back to prevoius scene");
     }
+
+    public void goHome(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
