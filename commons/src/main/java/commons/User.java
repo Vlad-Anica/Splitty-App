900bd509de00e591d2d5ee48507cf98093af7cab
Frank Verkoren <f.h.w.verkoren@student.tudelft.nl>
2024-03-10 17:46:08 +0100

    fixed checkstyle warnings

--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -1,8 +1,6 @@
 package commons;
 
 import jakarta.persistence.*;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.util.Objects;
 
