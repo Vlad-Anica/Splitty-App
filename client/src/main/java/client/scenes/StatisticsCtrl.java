4aef9ce294aeb275e14c5378887a8e326f612af2
Duco Lam <d.f.lam@student.tudelft.nl>
2024-03-10 15:52:58 +0100

    Added a piechart to the statistics page and filled it with placeholder stats.

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -1,4 +1,32 @@
 package client.scenes;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.*;
 
-public class StatisticsCtrl {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class StatisticsCtrl implements Initializable {
+
+@FXML
+PieChart PieChartExpenses;
+@FXML
+ObservableList<PieChart.Data> pieChartExpensesData =
+        FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+
+@Override
+public void initialize(URL location, ResourceBundle resources) {
+    PieChartExpenses.setData(pieChartExpensesData);
+}
+
+
 }
