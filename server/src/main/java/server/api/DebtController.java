c88c0c3ccab3dbf60d107f0505060c8b65fca559
Vlad Anica <V.I.Anica-Popa@student.tudelft.nl>
2024-03-17 14:06:42 +0100

    created the post mapping for adding a Debt to the database.

--- server/src/main/java/server/api/DebtController.java
+++ server/src/main/java/server/api/DebtController.java
@@ -1,13 +1,30 @@
 package server.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import commons.Person;
+import org.springframework.web.bind.annotation.*;
 import server.database.DebtRepository;
 import commons.Debt;
+import server.database.PersonRepository;
 
 @RestController
 @RequestMapping("/debt")
 public class DebtController {
+    private DebtRepository debtRep;
+    private PersonRepository personRep;
+    private ExpenseRepository expenseRep;
+    public DebtController(DebtRepository debtRep, PersonRepository personRep, ExpenseRepository expenseRep) {
+        this.debtRep = debtRep;
+        this.personRep = personRep;
+        this.expenseRep = expenseRep;
+    }
 
+    @PostMapping("create")
+    public Debt createDebt(@RequestParam("giver") Long giverId,
+                           @RequestParam("receiver") Long receiverId, @RequestParam("expense") Long expenseId,
+                           @RequestParam("amount") Double amount) {
+        Debt debt = new Debt(personRep.getReferenceById(giverId), personRep.getReferenceById(receiverId),
+                expenseRep,getReferenceById(expenseId), amount);
+        debtRep.save(debt);
+        return debt;
+    }
 }
