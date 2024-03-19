ce84bc43485e9e6a50cb74f00063665e5f1c4bed
Vlad Anica <V.I.Anica-Popa@student.tudelft.nl>
2024-03-19 01:31:55 +0100

    Add language change feature to settings.

--- client/src/main/java/client/scenes/SettingsCtrl.java
+++ client/src/main/java/client/scenes/SettingsCtrl.java
@@ -9,44 +9,49 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsCtrl {
-    @FXML
-    private ComboBox<String> selectedLanguage;
     @FXML
     private ComboBox selectedCurrency;
     @FXML
     private TextField publicUsername;
     @FXML
     private Button btnSubmitAll;
+    List<String> btnSubmitAllText = new ArrayList<>(List.of("Submit", "Submit (in Dutch)"));
     @FXML
     private Text settingsTitle;
+    List<String> settingsTitleText = new ArrayList<>(List.of("Settings", "Settings (in Dutch)"));
     @FXML
     private Button btnHome;
+    List<String> btnHomeText = new ArrayList<>(List.of("Home", "Home (in Dutch)"));
     private MainCtrl mainCtrl;
 
     private String currentUsername = "Sapunaru";
     private String currency = "EUR";
-    private String language = "English";
     @Inject
     public SettingsCtrl(MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
     }
 
     public void setup() {
-        selectedLanguage.setItems(FXCollections.observableList(List.of("English", "Dutch")));
         selectedCurrency.setItems(FXCollections.observableList(List.of("EUR", "USD", "CHF", "GBP")));
-        publicUsername.setText("Cristian Sapunaru #01");
-        btnSubmitAll.setText("Submit");
-        settingsTitle.setText("Settings");
+        selectedCurrency.getSelectionModel().select(currency);
+        publicUsername.setText(currentUsername);
         settingsTitle.setFont(Font.font("Verdana", 30));
-        btnHome.setText("Home");
+        setLanguageText();
     }
 
+    public void setLanguageText() {
+        int languageIndex = mainCtrl.getLanguageIndex();
+        btnSubmitAll.setText(btnSubmitAllText.get(languageIndex));
+        settingsTitle.setText(settingsTitleText.get(languageIndex));
+        btnHome.setText(btnHomeText.get(languageIndex));
+
+    }
     public void clickBtnSubmitAll() {
         currentUsername = publicUsername.getText();
-        language = (String)selectedLanguage.getValue();
         currency = (String)selectedCurrency.getValue();
     }
 
