97dbe28ea9eff7fc8fca934266053534cfd9fd0d
Victor Clătici <v.e.clatici@student.tudelft.nl>
2024-03-19 14:59:11 +0100

    Fixed pipelining fail.

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 //import server.database.PersonRepository;
-import server.database.DebtRepository;
 import server.database.EventRepository;
 import server.database.ExpenseRepository;
 import server.database.PersonRepository;
