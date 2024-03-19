4fb384f507384d0c338db5e574b33d8879a11955
Victor Clătici <v.e.clatici@student.tudelft.nl>
2024-03-19 11:44:53 +0100

    Added basic constructors for Event, Expense. Reformatted some code.

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 //import server.database.PersonRepository;
+import server.database.DebtRepository;
 import server.database.ExpenseRepository;
+import server.database.PersonRepository;
 //import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 //import java.util.Map;
 
@@ -17,34 +20,73 @@ import java.util.List;
 public class ExpenseController {
 
     @Autowired
-    private final ExpenseRepository db;
-    public ExpenseController(ExpenseRepository db) {
+    private final ExpenseRepository expenseRep;
+    private PersonRepository personRep;
+    private DebtRepository debtRep;
 
-        this.db = db;
+    /**
+     * Creates a new ExpenseController
+     *
+     * @param expenseRep repository for Expense
+     * @param personRep  repository for Person
+     * @param debtRep    repository for Debt
+     */
+    public ExpenseController(ExpenseRepository expenseRep, PersonRepository personRep, DebtRepository debtRep) {
+        this.expenseRep = expenseRep;
+        this.personRep = personRep;
+        this.debtRep = debtRep;
     }
 
-    @GetMapping(path = { "", "/" })
+    /**
+     * Creates a corresponding Expense based on the given parameters. In normal usage, debtIDs should be empty...
+     * @param description description to use for the Expense
+     * @param amount amount spend on the Expense
+     * @param date date the Expense took place
+     * @param receiverID ID of the one who footed the bill
+     * @param debtIDs IDs of the respective debts associated with the Expense
+     * @param currency Currency the Expense was executed in
+     * @param tag Tag associated with the expense
+     * @return the Expense object created
+     */
+    @PostMapping("/")
+    public Expense createExpense(@RequestParam("description") String description,
+                                 @RequestParam("amount") Double amount,
+                                 @RequestParam("date") Date date,
+                                 @RequestParam("receiver") Long receiverID,
+                                 @RequestParam("debts") List<Long> debtIDs,
+                                 @RequestParam("currency") Currency currency,
+                                 @RequestParam("tag") Tag tag) {
+        ArrayList<Debt> debts = new ArrayList<>();
+        for (Long dID : debtIDs) {
+            debts.add(debtRep.getReferenceById(dID));
+        }
+        Expense expense = new Expense(description, amount, date, personRep.getReferenceById(receiverID), debts, currency, tag);
+        expenseRep.save(expense);
+        return expense;
+    }
+
+    @GetMapping(path = {"", "/"})
     public List<Expense> getAll() {
         System.out.println("Find people...");
-        return db.findAll();
+        return expenseRep.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Expense> getById(@PathVariable("id") long id) {
 
-        if (id < 0 || !db.existsById(id)) {
+        if (id < 0 || !expenseRep.existsById(id)) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(db.findById(id).get());
+        return ResponseEntity.ok(expenseRep.findById(id).get());
     }
 
 
     @GetMapping("participants/{id}")
-    public List<Debt> getParticipantsById(@PathVariable("id") long id){
-        if (id < 0 || !db.existsById(id)) {
+    public List<Debt> getParticipantsById(@PathVariable("id") long id) {
+        if (id < 0 || !expenseRep.existsById(id)) {
             return null;
         }
-        return db.findById(id).get().getDebtList();
+        return expenseRep.findById(id).get().getDebtList();
     }
 
     private static boolean isNullOrEmpty(String s) {
