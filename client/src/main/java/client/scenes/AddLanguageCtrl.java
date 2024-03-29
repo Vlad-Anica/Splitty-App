17d3eef9b9e36ec7aa18cc37837e3587957ab818
Frank Verkoren <f.h.w.verkoren@student.tudelft.nl>
2024-03-29 12:12:53 +0100

    Removed ColumnX and fixed a column name

--- client/src/main/java/client/scenes/AddLanguageCtrl.java
+++ client/src/main/java/client/scenes/AddLanguageCtrl.java
@@ -102,13 +102,12 @@ public class AddLanguageCtrl{
         languageName.setText(resourceBundle.getString("LanguageName"));
         goHomeButton.setText(resourceBundle.getString("Home"));
         addButton.setText(resourceBundle.getString("Add"));
-        languageColumn.setText(mainCtrl.getLanguage());
+        languageColumn.setText(mainCtrl.getLanguage().split(";")[0]);
         yourLanguageColumn.setText(resourceBundle.getString("YourLanguage"));
     }
     public HashMap<String, String> getPhrases() {
         HashMap<String, String> result = new HashMap<>();
         try {
-            String language = mainCtrl.getLanguage();
             File file = new File("client/src/main/resources/languages/language_" + mainCtrl.getLanguageWithoutImagePath() + ".properties");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
