1768f262390c1f287f95abeab180f9f409b33735
Vlad Anica <V.I.Anica-Popa@student.tudelft.nl>
2024-03-30 18:22:35 +0100

    Fix checkstyle errors.

--- client/src/main/java/client/scenes/SeeEventsAsAdminCtrl.java
+++ client/src/main/java/client/scenes/SeeEventsAsAdminCtrl.java
@@ -13,7 +13,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
