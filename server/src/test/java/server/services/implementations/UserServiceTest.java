4e19a60c5ebe25c1a6816ce29981bf3e7d8034e3
Lucca Furlan Piva Demarzo <L.FurlanPivaDemarzo@student.tudelft.nl>
2024-03-31 23:47:13 +0200

    imports

--- server/src/test/java/server/services/implementations/UserServiceTest.java
+++ server/src/test/java/server/services/implementations/UserServiceTest.java
@@ -15,7 +15,7 @@ import server.services.interfaces.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
