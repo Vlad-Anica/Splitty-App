5adb09a198e1088f219625a84ef8cd203cf14917
Calin Olaru <c.m.olaru@student.tudelft.nl>
2024-04-07 16:54:02 +0200

    re-added import that was causing errors after being deleted

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -31,6 +31,7 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URI;
