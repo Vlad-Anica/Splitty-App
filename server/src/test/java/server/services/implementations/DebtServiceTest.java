cbc15c93cdd634c36c6c3d73cd3d1aa6e7269056
Vlad Anica <V.I.Anica-Popa@student.tudelft.nl>
2024-04-11 18:07:53 +0200

    Remove unused imports.

--- server/src/test/java/server/services/implementations/DebtServiceTest.java
+++ server/src/test/java/server/services/implementations/DebtServiceTest.java
@@ -5,7 +5,6 @@ import commons.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
