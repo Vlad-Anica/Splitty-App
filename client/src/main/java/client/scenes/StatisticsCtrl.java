291a66c989c3fe5648d95fc0a72ca5cab79c9a78
Frank Verkoren <f.h.w.verkoren@student.tudelft.nl>
2024-04-13 22:26:22 +0200

    Added two more piecharts for money owed and added it so that the label follows the mouse

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -1,7 +1,9 @@
 package client.scenes;
+
 import client.utils.ServerUtils;
 import commons.Event;
 import commons.Expense;
+import commons.Person;
 import commons.Tag;
 import jakarta.inject.Inject;
 import javafx.collections.FXCollections;
@@ -24,36 +26,46 @@ import java.util.ResourceBundle;
 public class StatisticsCtrl implements Initializable {
 
 
-private final EventOverviewCtrl eventController;
-private final MainCtrl mainCtrl;
-private Event event;
-private String eventTitle;
-private ServerUtils server;
-private ArrayList<Tag> tags;
-private ArrayList<Expense> allExpenses;
-private double totalAmount;
-private boolean check = false;
+    private final EventOverviewCtrl eventController;
+    private final MainCtrl mainCtrl;
+    private Event event;
+    private String eventTitle;
+    private ServerUtils server;
+    private ArrayList<Tag> tags;
+    private ArrayList<Expense> allExpenses;
+    private double totalAmount;
+    private boolean check = false;
 
-private long eventId;
+    private long eventId;
 
-@FXML
-Button temporaryButton;
+    @FXML
+    Button temporaryButton;
 
-@FXML
-private Label totalExpensesLabel;
+    @FXML
+    private Label totalExpensesLabel;
 
-@FXML
-PieChart PieChartExpenses;
-@FXML
-Label pieChartDataLabel;
-@FXML
-Button goBackButton;
-@FXML
-ObservableList<PieChart.Data> pieChartExpensesData =
-        FXCollections.observableArrayList();
+    @FXML
+    PieChart PieChartExpenses;
+    @FXML
+    PieChart PieChartPersons;
+    @FXML
+    PieChart PieChartPersons1;
+    @FXML
+    Label pieChartDataLabel;
+    @FXML
+    Button goBackButton;
+    @FXML
+    ObservableList<PieChart.Data> pieChartExpensesData =
+            FXCollections.observableArrayList();
+    @FXML
+    ObservableList<PieChart.Data> pieChartPersonsData =
+            FXCollections.observableArrayList();
+    @FXML
+    ObservableList<PieChart.Data> pieChartPersonsData1 =
+            FXCollections.observableArrayList();
 
     @Inject
-    public StatisticsCtrl(EventOverviewCtrl eventController, ServerUtils server, MainCtrl mainCtrl){
+    public StatisticsCtrl(EventOverviewCtrl eventController, ServerUtils server, MainCtrl mainCtrl) {
         this.eventController = eventController;
         this.server = server;
         this.mainCtrl = mainCtrl;
@@ -77,35 +89,61 @@ ObservableList<PieChart.Data> pieChartExpensesData =
         loadExpenses(eventId);
     }
 
-    public void loadExpenses(Long eventId){
-        try{
+    public void loadExpenses(Long eventId) {
+        try {
             allExpenses = (ArrayList<Expense>) event.getExpenses();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("An error has occured when fetching the expenses!");
         }
-        HashMap<Tag, Double> map = new HashMap<Tag, Double>();
-        try{
-            for (Expense e : allExpenses){
-                map.merge(e.getTag(), e.getAmount(), Double::sum);
+        HashMap<Tag, Double> tagMap = new HashMap<>();
+        HashMap<Person, Double> owedMap = new HashMap<>();
+        try {
+            for (Expense e : allExpenses) {
+                tagMap.merge(e.getTag(), e.getAmount(), Double::sum);
+                Person receiver = e.getReceiver();
+                owedMap.merge(receiver, e.getAmount(), Double::sum);
+                List<Person> givers = e.getGivers();
+                for (Person p : givers) {
+                    owedMap.merge(p, -1 * e.getAmount() / givers.size(), Double::sum);
+                }
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Whoopsie, an error has occured!");
             return;
         }
         //pieChartExpensesData.clear();
-        try{
-            map.forEach((tag, amount)->{
+        try {
+            tagMap.forEach((tag, amount) -> {
                 String tagName = tag.getType();
                 boolean checker = false;
-                for (PieChart.Data piedata : pieChartExpensesData){
-                    if (piedata.getName().equals(tagName)){
+                for (PieChart.Data piedata : pieChartExpensesData) {
+                    if (piedata.getName().equals(tagName)) {
                         checker = true;
                         if (piedata.getPieValue() != amount) piedata.setPieValue(amount);
                     }
                 }
                 if (!checker) pieChartExpensesData.add(new PieChart.Data(tagName, amount));
             });
-        } catch (Exception e){
+            owedMap.forEach((person, amount) -> {
+                String name = person.getFirstName() + " " + person.getLastName();
+                boolean checker = false;
+                for (PieChart.Data piedata : pieChartPersonsData) {
+                    if (piedata.getName().equals(name)) {
+                        checker = true;
+                        if (piedata.getPieValue() != amount && amount > 0) piedata.setPieValue(amount);
+                    }
+                }
+                if (!checker && amount > 0) pieChartPersonsData.add(new PieChart.Data(name, amount));
+                checker = false;
+                for (PieChart.Data piedata : pieChartPersonsData1) {
+                    if (piedata.getName().equals(name)) {
+                        checker = true;
+                        if (piedata.getPieValue() != amount && amount < 0) piedata.setPieValue(-amount);
+                    }
+                }
+                if (!checker && amount < 0) pieChartPersonsData1.add(new PieChart.Data(name, -amount));
+            });
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println(e.getCause());
             System.out.println("An error occured when processing the tags!");
@@ -113,15 +151,43 @@ ObservableList<PieChart.Data> pieChartExpensesData =
         }
 
         for (final PieChart.Data data : PieChartExpenses.getData()) {
-            data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
+            data.getNode().addEventHandler(MouseEvent.MOUSE_MOVED,
                     e -> {
                         double total = 0;
                         for (PieChart.Data d : PieChartExpenses.getData()) {
                             total += d.getPieValue();
                         }
-                        pieChartDataLabel.setTranslateX(e.getSceneX() - mainCtrl.getPrimaryStage().getWidth()/4);
-                        pieChartDataLabel.setTranslateY(e.getSceneY() - mainCtrl.getPrimaryStage().getHeight()/2.5);
-                        String text = data.getPieValue() + "(" + String.format("%.1f%%", 100*data.getPieValue()/total) + "%)" ;
+                        pieChartDataLabel.setTranslateX(e.getSceneX() - mainCtrl.getPrimaryStage().getWidth() / 4);
+                        pieChartDataLabel.setTranslateY(e.getSceneY() - mainCtrl.getPrimaryStage().getHeight() / 2.5);
+                        String text = data.getName() + ": \n" + data.getPieValue() + "(" + String.format("%.1f%%", 100 * data.getPieValue() / total) + ")";
+                        pieChartDataLabel.setText(text);
+                    }
+            );
+        }
+        for (final PieChart.Data data : PieChartPersons.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_MOVED,
+                    e -> {
+                        double total = 0;
+                        for (PieChart.Data d : PieChartPersons.getData()) {
+                            total += d.getPieValue();
+                        }
+                        pieChartDataLabel.setTranslateX(e.getSceneX() - mainCtrl.getPrimaryStage().getWidth() / 3);
+                        pieChartDataLabel.setTranslateY(e.getSceneY() - mainCtrl.getPrimaryStage().getHeight() / 2.5);
+                        String text = data.getName() + ": \n" + data.getPieValue() + "(" + String.format("%.1f%%", 100 * data.getPieValue() / total) + ")";
+                        pieChartDataLabel.setText(text);
+                    }
+            );
+        }
+        for (final PieChart.Data data : PieChartPersons1.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_MOVED,
+                    e -> {
+                        double total = 0;
+                        for (PieChart.Data d : PieChartPersons1.getData()) {
+                            total += d.getPieValue();
+                        }
+                        pieChartDataLabel.setTranslateX(e.getSceneX() - mainCtrl.getPrimaryStage().getWidth() / 3);
+                        pieChartDataLabel.setTranslateY(e.getSceneY() - mainCtrl.getPrimaryStage().getHeight() / 2.5);
+                        String text = data.getName() + ": \n" + data.getPieValue() + "(" + String.format("%.1f%%", 100 * data.getPieValue() / total) + ")";
                         pieChartDataLabel.setText(text);
                     }
             );
@@ -138,18 +204,20 @@ ObservableList<PieChart.Data> pieChartExpensesData =
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         PieChartExpenses.setData(pieChartExpensesData);
+        PieChartPersons.setData(pieChartPersonsData);
+        PieChartPersons1.setData(pieChartPersonsData1);
     }
 
 
-    public void setTotalExpenses(){
+    public void setTotalExpenses() {
         List<Expense> allExpenses = event.getExpenses();
         totalAmount = 0;
-        for (Expense expense : allExpenses){
+        for (Expense expense : allExpenses) {
             totalAmount += expense.getAmount();
         }
     }
 
-    public void goHome(ActionEvent event){
+    public void goHome(ActionEvent event) {
         mainCtrl.showEventOverview(eventId);
     }
 
